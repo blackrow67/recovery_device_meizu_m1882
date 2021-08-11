3436ca578a6792a68d931de8c6d4f453456a5204
blackrow67 <dogukanbey42@gmail.com>
2021-08-11 09:42:32 +0000

    m1882: pbrp

--- omni_m1882.mk
+++ omni_m1882.mk
@@ -14,10 +14,8 @@
 # limitations under the License.
 #
 
-$(call inherit-product, build/target/product/embedded.mk)
-
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 PRODUCT_NAME := omni_m1882
 PRODUCT_BRAND := Meizu
